4c09894c9c8f8027aa43f27be1bc7b1260589249
claudiahW <murungiclaud@gmail.com>
2024-10-15 15:38:19 +0300

    center hero section

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Hero.css";
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="hero-text">
+        <h1>We ensure better education for a better world</h1>
+        <p>
+          Our cutting edge curriculum is designed to emopower students with the
+          knowledge , skills and experiences designed to excel.
+        </p>
+        <button className="btn">Explore more</button>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
