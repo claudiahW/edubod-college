3362bc11c4f545d150fd75a98f652268e9ab9b1b
claudiahW <murungiclaud@gmail.com>
2024-10-16 19:00:00 +0300

    mount testimonial component to app component

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Programs from "./Components/Programs/Programs";
 import Title from "./Components/Title/Title";
 import About from "./Components/About/About";
 import Campus from "./Components/Campus/campus";
+import Testimonials from "./Components/Testimonials/Testimonials";
 
 const App = () => {
   return (
@@ -18,6 +19,8 @@ const App = () => {
         <Title subTitle="Gallery" title="Campus Photos" />
 
         <Campus />
+        <Title subTitle="TESTIMONIALS" title="What Students Say" />
+        <Testimonials />
       </div>
     </div>
   );
--- src/Components/Campus/Campus.jsx
+++ src/Components/Campus/Campus.jsx
@@ -5,8 +5,9 @@ import gallery_1 from "../../assets/gallery-1.png";
 import gallery_2 from "../../assets/gallery-2.png";
 import gallery_3 from "../../assets/gallery-3.png";
 import gallery_4 from "../../assets/gallery-4.png";
+import white_arrow from "../../assets/white-arrow.png";
 
-const campus = () => {
+const Campus = () => {
   return (
     <div className="campus">
       <div className="gallery">
@@ -15,8 +16,12 @@ const campus = () => {
         <img src={gallery_3} alt="" />
         <img src={gallery_4} alt="" />
       </div>
+
+      <button className="btn dark-btn">
+        See more here <img src={white_arrow} alt="" />
+      </button>
     </div>
   );
 };
 
-export default campus;
+export default Campus;
