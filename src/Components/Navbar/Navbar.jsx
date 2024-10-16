60a2b0d4958ce454dbf6dc7617874d01240d5581
claudiahW <murungiclaud@gmail.com>
2024-10-16 16:32:25 +0300

    add background color to nav bar when scrolled

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Navbar.css";
 import logo from "../../assets/logo.png";
 
 const Navbar = () => {
+  const [sticky, setSticky] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      window.scrollY > 50 ? setSticky(true) : setSticky(false);
+    });
+  }, []);
   return (
-    <nav className="container">
+    <nav className={`container ${sticky ? "dark-nav" : ""}`}>
       <img src={logo} alt="" className="logo" />
       <ul>
         <li>Home</li>
@@ -12,8 +19,9 @@ const Navbar = () => {
         <li>About Us</li>
         <li>Campus</li>
         <li>Testimonials</li>
-        <li> 
-          <button className="btn">Contact Us</button></li>
+        <li>
+          <button className="btn">Contact Us</button>
+        </li>
       </ul>
     </nav>
   );
